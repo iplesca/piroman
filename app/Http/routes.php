3901c80b8c30f6b28d89cd53786f914a8483e885
iplesca <sirlucas_ro@yahoo.com>
2016-09-13 11:14:02 +0100

    Adaugat linkuri simple

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,11 @@ Route::get('/', function () {
 Route::get('/regulament.html', function () { 
     return view('regulament');
 });
+Route::get('/clasament.html', function () {
+    $gDoc = 'https://docs.google.com/spreadsheets/d/1RdHBKhmb5wqn9nNvoLdZdrv8j5mN-rMAOdzcK3KZxUo/pubhtml?gid=0&single=true';
+    return redirect($gDoc);
+});
+Route::get('/turneu.html', function () {
+    $gDoc = 'https://docs.google.com/spreadsheets/d/15RfvekijMpMI73ESfYtZz8ZwL9IcNyD7Ikq2_IvKaVI/pubhtml?gid=0&single=true';
+    return redirect($gDoc);
+});
